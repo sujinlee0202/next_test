a89848ac85367cfa219eb643da58feb161d840a5
titch <sujinlee0202@gmail.com>
2024-02-23 16:58:28 +0900

    API Routes 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -89,7 +89,11 @@ const Home: NextPage<Props> = ({ stores }) => {
 export default Home;
 
 export async function getStaticProps() {
+  /** TODO: next api routes로 불러오기 */
   const stores = (await import('../public/stores.json')).default;
+  // const stores = await fetch(
+  //   `${process.env.NEXT_PUBLIC_API_URL}/api/stores`,
+  // ).then((res: Response) => res.json());
 
   return {
     props: { stores },
